b326b58b4f23fe4546f6cca0ac2e5dce99cb6757
Tina <k.a.kyrova@tmn3.etagi.com>
2021-06-06 12:09:11 +0500

    6

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const auth = require('./auth');
-
 const fileUpload = require('express-fileupload');
 const apiRoute = require('./routes/api.routes');
-const authRoute = require('./routes/auth.routes');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -14,12 +10,7 @@ app.use(express.json({type: 'application/vnd.api+json'}));
 app.use(cors());
 app.use(fileUpload());
 app.use('/public', express.static(__dirname + '/public'));
-app.use('/auth/', authRoute);
-
 app.use('/api', apiRoute);
-
-//проверять авторизацию нужно только на определенных роутах
-// app.use('/api/', auth.verifyToken, apiRoute);
 
 
 app.listen(3003, function () {
