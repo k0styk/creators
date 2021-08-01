b0b5de605b03ba455a7f2b20b8d312411db67ca9
Konstantin <rimshakonstantin@ya.ru>
2021-08-01 15:21:10 +0500

    back features for env variables

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
-const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const app = express();
@@ -10,17 +9,26 @@ const { session, headers } = require('./middleware');
 //Setup Error Handlers -- MUST BE LAST USE DIRECTIVES
 const errorHandlers = require('./handlers/errorHandlers');
 
+const origin = process.env['ORIGIN']?.split(' ') || [
+    'http://socket.test',
+    'http://localhost:3000',
+];
+const compress = process.env['COMPRESSION'] === 'true';
+
 app.enable('trust proxy');
 app.use(headers);
 app.use(
     cors({
-        origin: ['http://socket.test', 'http://localhost:3000'],
+        origin,
         preflightContinue: true,
         credentials: true,
     })
 );
 app.use(helmet());
-app.use(compression());
+if (compress) {
+    const compression = require('compression');
+    app.use(compression());
+}
 app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(express.urlencoded({ extended: true }));
