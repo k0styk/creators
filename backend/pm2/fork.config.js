0d2034dfb97daeb5259ff704cc9efadd43bb1bb0
Konstantin <rimshakonstantin@ya.ru>
2021-10-27 16:48:18 +0500

    env fixing

--- backend/pm2/fork.config.js
+++ backend/pm2/fork.config.js
@@ -1,20 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: 'api_app',
+      name: 'test_api_app',
       script: './server.js',
-      watch: ['./'],
-      ignore_watch: [
-        './.logs',
-        './migrations',
-        './node_modules',
-        './.env',
-        './.nginx.conf',
-        './.gitignore',
-        './packae.json',
-        './packae-lock.json',
-      ],
-      watch_delay: 5000,
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
       merge_logs: true,
       error_file: '.logs/err.log',
@@ -22,7 +10,7 @@ module.exports = {
       max_restarts: 4,
       restart_delay: 5000,
       env: {
-        NODE_ENV: 'development',
+        NODE_ENV: 'production',
       },
       shutdown_with_message: true,
     },
